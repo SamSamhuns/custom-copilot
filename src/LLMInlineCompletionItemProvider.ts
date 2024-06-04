c477c6045784e7c683b1857bd8f25ed71bef0244
SamSamhuns <samhunsadamant@gmail.com>
2024-06-04 16:56:08 +0400

    Add llm inline completion provider

---
+++ src/LLMInlineCompletionItemProvider.ts
@@ -0,0 +1,42 @@
+import * as vscode from 'vscode';
+
+type ApiResponse = {
+    results: Array<{ text: string }>;
+};
+
+export class LLMInlineCompletionItemProvider implements vscode.InlineCompletionItemProvider {
+    async provideInlineCompletionItems(document: vscode.TextDocument, position: vscode.Position): Promise<vscode.InlineCompletionList> {
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+        const completionItems: vscode.InlineCompletionItem[] = [];
+        const textUpToCursor = document.getText(new vscode.Range(new vscode.Position(0, 0), position));
+        const LLM_API_URL = 'http://localhost:8002/test_api';
+        
+        try {
+            const response = await fetch(LLM_API_URL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ prompt: textUpToCursor }),
+            });
+            
+            // note response must be of fmt {"results": [{ "text": "RESULT_TEXT" }]}
+            const json = await response.json() as ApiResponse;
+            const predictions = json.results;
+            
+            for (const prediction of predictions) {
+                const code = prediction.text.trim();
+                const completionText = code;
+                const completionRange = new vscode.Range(position, position.translate(0, completionText.length));
+                completionItems.push({
+                    insertText: completionText,
+                    range: completionRange
+                });
+            }
+        } catch (err) {
+            console.error('Error while calling LLM API call:', err);
+        }
+
+        return { items: completionItems };
+    }
+}
