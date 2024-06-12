328a69e58f6566fecaf8e764d7c31862d1c87c59
SamSamhuns <samhunsadamant@gmail.com>
2024-06-12 15:50:09 +0400

    Provide autocomplete after cursor delay

--- src/LLMInlineCompletionItemProvider.ts
+++ src/LLMInlineCompletionItemProvider.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import { config } from './config'; 
+import { config } from './config';
 
 
 type ApiResponse = {
@@ -7,24 +7,38 @@ type ApiResponse = {
 };
 
 export class LLMInlineCompletionItemProvider implements vscode.InlineCompletionItemProvider {
+    private lastKeyPressedTime: number = Date.now();
+    private delay: number = config.autocompleteDelay; // in milliseconds
+
+    constructor() {
+        vscode.workspace.onDidChangeTextDocument(event => {
+            if (event.contentChanges.length > 0) {
+                this.lastKeyPressedTime = Date.now();
+            }
+        });
+    }
+
     async provideInlineCompletionItems(document: vscode.TextDocument, position: vscode.Position): Promise<vscode.InlineCompletionList> {
+        const currentTime = Date.now();
+        if (currentTime - this.lastKeyPressedTime < this.delay) {
+            await new Promise(resolve => setTimeout(resolve, this.delay - (currentTime - this.lastKeyPressedTime)));
+        }
+
         process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
         const completionItems: vscode.InlineCompletionItem[] = [];
         const textUpToCursor = document.getText(new vscode.Range(new vscode.Position(0, 0), position));
-        
+
         try {
             const response = await fetch(config.autocompleteAPIURL, {
                 method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
+                headers: { 'Content-Type': 'application/json', },
                 body: JSON.stringify({ prompt: textUpToCursor }),
             });
-            
+
             // note response must be of fmt {"results": [{ "text": "RESULT_TEXT" }]}
             const json = await response.json() as ApiResponse;
             const predictions = json.results;
-            
+
             for (const prediction of predictions) {
                 const code = prediction.text.trim();
                 const completionText = code;
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // src/config.ts
 export const config = {
     uploadAPIURL: 'http://localhost:8002/upload',
-    autocompleteAPIURL: 'http://localhost:8002/autocomplete'
+    autocompleteAPIURL: 'http://localhost:8002/autocomplete',
+    autocompleteDelay: 3000 // autocomplete delay in milli secs
 };
